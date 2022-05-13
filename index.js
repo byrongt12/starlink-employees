75b490d9d349e1c050f566dc316bab5a7b4f05c8
Byron Tomkinson <48924346+byrongt12@users.noreply.github.com>
2022-05-14 01:39:13 +0200

    server access changes

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ if (process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "client/build")))
 }
 
-//app.use(cors());
+
 app.use(express.json())
+
+
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
+
 
 app.get('/employees', (req, res) => {
     employee_model.getEmployees()
